08d563cc4fd4cb243dfc3c87f09d2f17659e35b0
colin <cosborn@patientslikeme.com>
2021-05-18 18:40:54 -0600

    passing tests and a few different implementations

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+// This one below works... but isn't as clean
+// function reverse(str) {
+//   var letters = str.split('');
+//   var reversed = letters.reverse();
+//   var joinArray = reversed.join('');
+//   return joinArray
+// }
+
+function reverse(str) {
+  return str.split('').reverse().join('')
+}
+
+// now with recrusion...
+
+function reverse(str) {
+  if (str === "")
+    return "";
+  else
+    return reverse(str.substr(1)) + str.charAt(0); 
+}
 
 module.exports = reverse;
+
+// These all work.... ideally the recursion would be good... but better really know it
